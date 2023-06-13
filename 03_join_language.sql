4bd0411a1083c0629e702a73618be6eb91c2558f
mullrich192 <melanie.ullrich@smail.th-koeln.de>
2023-06-13 20:33:30 +0000

    prepare code for bash script

---
+++ 03_join_language.sql
@@ -0,0 +1,12 @@
+-- ke_stage.join_language source
+
+CREATE MATERIALIZED VIEW ke_stage.join_language
+TABLESPACE pg_default
+AS SELECT cs.dbrecordid,
+    cs.title,
+    cs.abstract,
+    cl.lang_title,
+    cl.lang_abs
+   FROM ke_stage.corpus_small cs
+     JOIN ke_stage.corpus_language cl ON cs.dbrecordid = cl.dbrecordid
+WITH DATA;
